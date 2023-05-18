84942bbe143ec038089d59ac3de652a242cca156
zabaidulbd <zislam5622@gmail.com>
2023-05-18 11:57:29 +0600

    add banner section

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <div>
+                    <img className="w-36 ms-16 me-5 " src="https://img.freepik.com/free-vector/construction-background-design_1343-2.jpg?w=740&t=st=1684385038~exp=1684385638~hmac=1a9d333d2e08e353a4a512bab8f7a56a81a751eb664067e63751fbd53df949af" alt="" />
+                </div>
+                <div><h1 className="text-5xl">Lego-Store</h1></div>
+                <div>
+                    <span className="text-2xl ms-5"><Link to={'/'}>Home</Link></span>
+                </div>
+                <div>
+                    <span className="text-2xl mx-3"><Link to={'/'}>All-Toys</Link></span>
+                </div>
+                <div>
+                    <span className="text-2xl"><Link to={'/'}>Blog</Link></span> </div>
+            </div>
+            <div className="flex-none gap-2">
+                <div>
+                    <span className="text-2xl mx-3"><Link to={'/'}>Add a Toy</Link></span>
+                </div>
+                <div>
+                    <span className="text-2xl"><Link to={'/'}>My Toys</Link></span>
+                </div>
+
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <img src="https://img.freepik.com/free-photo/young-man-wearing-blue-outfit-holding-red-folder_1298-179.jpg?w=740&t=st=1683177286~exp=1683177886~hmac=c440209e285a379ced974449f84436bdcc297217e39170b08f534dfe769c131d" />
+                    </div>
+                </label>
+                <div>
+                    <button className="btn btn-active btn-accent">Login</button>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Navbar;
