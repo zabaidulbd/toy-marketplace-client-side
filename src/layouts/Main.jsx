48258ba6c663c7fe9726364cd439f0e0848a0480
zabaidulbd <zislam5622@gmail.com>
2023-05-18 12:40:12 +0600

    add footer

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
 import TitlePage from "../shared/TitlePage/TitlePage";
+import Footer from "../shared/Footer/Footer";
 
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
             <TitlePage></TitlePage>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
