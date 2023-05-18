8e7e096d6c4f77012e1770b43b8ad070b87691ff
zabaidulbd <zislam5622@gmail.com>
2023-05-18 12:05:57 +0600

    add title page

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
+import TitlePage from "../shared/TitlePage/TitlePage";
 
 
 const Main = () => {
     return (
         <div>
+            <TitlePage></TitlePage>
             <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
---
+++ src/shared/TitlePage/TitlePage.jsx
@@ -0,0 +1,12 @@
+
+
+const TitlePage = () => {
+    return (
+        <div className="text-center mt-10">
+            <h1 className='text-7xl font-extrabold'>Lego</h1>
+            <h5 className='text-3xl font-semibold mt-3'>Store</h5>
+        </div>
+    );
+};
+
+export default TitlePage;
