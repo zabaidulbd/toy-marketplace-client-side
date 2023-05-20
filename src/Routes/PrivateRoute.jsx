202a34dc488a6ce0399e145766e50eeb6c77fa1d
zabaidulbd <zislam5622@gmail.com>
2023-05-20 20:24:52 +0600

    add use loction

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
-import { Navigate } from 'react-router';
+import { Navigate, useLocation } from 'react-router';
 
 const PrivateRoute = ({ children }) => {
 
     const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
 
     if (loading) {
         return <progress className="progress w-56"></progress>;
@@ -15,7 +16,7 @@ const PrivateRoute = ({ children }) => {
     }
 
 
-    return <Navigate to={'/login'}></Navigate>
+    return <Navigate state={{ from: location }} to='/login' replace></Navigate >
 };
 
 export default PrivateRoute;
