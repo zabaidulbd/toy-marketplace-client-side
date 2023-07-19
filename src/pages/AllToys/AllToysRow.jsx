d9ffcd0218ff6384f4b3dc0cc9df6ed67b18a9a1
zabaidulbd <zislam5622@gmail.com>
2023-07-19 17:04:07 +0600

    add toy details sucessfully

--- src/pages/AllToys/AllToysRow.jsx
+++ src/pages/AllToys/AllToysRow.jsx
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom";
 
 const AllToysRow = ({ singleToy }) => {
 
-    const { _id, name, sellerName, subCategory, price, quantity } = singleToy;
+    const { _id, name, subCategory, price, quantity } = singleToy;
 
 
     return (
         <>
-            <tr>
-                <td>{sellerName}</td>
+            <tr className="font-semibold">
+                <td></td>
+                <td></td>
                 <td>{name}</td>
                 <td>{subCategory}</td>
                 <td>{price}</td>
+                <td></td>
                 <td>{quantity}</td>
                 <td>
-                    <Link to={`/toyDetails/${_id}`}><button className="btn btn-outline btn-warning">Details</button></Link>
+                    <Link to={`/toyDetails/${_id}`}><button className="border-solid bg-gray-300 hover:bg-gray-400 p-4">Details</button></Link>
 
                 </td>
             </tr>
--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -11,23 +11,22 @@ const ToyDetails = () => {
     return (
         <>
             <Navbar></Navbar>
-            <h1 className='text-center font-bold text-5xl mt-10 mb-20 text-orange-400'>Details of a Single Toy</h1>
+            <h1 className='text-center font-bold text-5xl mt-10 mb-20'>Details of a Single Toy</h1>
             <div className="card w-96 bg-base-100 shadow-xl my-10 mx-auto">
                 <figure><img src={picture} alt="Toy Image" /></figure>
                 <div className='card-body'>
                     <div className='flex justify-between'>
-                        <h2 className="card-title text-orange-400">Name : {name}</h2>
-                        <h2 className="card-title text-orange-400">Seller name : {sellerName}</h2>
+                        <h2 className="card-title">Name : {name}</h2>
+                        <h2 className="card-title">Seller name : {sellerName}</h2>
                     </div>
-                    <p className='text-center font-bold text-orange-400'>Email : {email}</p>
+                    <p className='text-center font-bold'>Email : {email}</p>
                     <div className='flex justify-between text-center'>
-                        <p className='text-orange-400'><strong>Price :</strong> {price}</p>
-                        <p className='text-orange-400'><strong>Rating :</strong> {rating}</p>
-                        <p className='text-orange-400'><strong>Quantity :</strong> {quantity}</p>
+                        <p><strong>Price :</strong> {price}</p>
+                        <p><strong>Rating :</strong> {rating}</p>
+                        <p><strong>Quantity :</strong> {quantity}</p>
                     </div>
-                    <p className='text-center text-orange-400'><strong>Details :</strong>  {detail}</p>
+                    <p className='text-center'><strong>Details :</strong>  {detail}</p>
                 </div>
-
             </div>
             <Footer></Footer>
         </>
