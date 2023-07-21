b7a88f06d301cc60770731e82abc070c9c419936
zabaidulbd <zislam5622@gmail.com>
2023-07-21 11:35:11 +0600

    add animation in login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,8 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.config";
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "../../assets/lottie/animation_lkc55ybv.json";
 
 
 const Login = () => {
@@ -52,34 +54,44 @@ const Login = () => {
     return (
         <>
             <Navbar></Navbar>
-            <form onSubmit={handleLogin}>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-10 ms-40">
-                    <h1 className="text-center text-3xl font-bold mt-10 text-pink-500">Please Login!</h1>
-                    <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text text-pink-600">Email</span>
-                            </label>
-                            <input type="text" name="email" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text text-pink-600">Password</span>
-                            </label>
-                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <p className="label-text-alt text-sm text-pink-500">Do not have account ? <span className="text-black font-bold"><Link to={'/register'}>Register</Link></span> </p>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button onClick={handleGoogleLogIn} className="btn btn-outline btn-secondary"> <FaGooglePlus style={{ fontSize: '30px', marginRight: '10px' }}></FaGooglePlus>Log In</button>
-                            <input className="btn btn-secondary mt-2" type="submit" value="Login" />
-                        </div>
-                        <p className='text-red-600'>{logInError}</p>
+            <div className="flex flex-col items-center">
+                <div className="flex">
+                    {/* Left side - Image */}
+                    <div className="w-1/2 p-4">
+                        <Lottie animationData={groovyWalkAnimation} loop={true} />
                     </div>
-
+                    {/* Right side - Login Form */}
+                    <div className="w-1/2 p-4 ms-9 mt-16">
+                        <form onSubmit={handleLogin}>
+                            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                                <h1 className="text-center text-3xl font-bold mt-10">Please Login!</h1>
+                                <div className="card-body">
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Email</span>
+                                        </label>
+                                        <input type="text" name="email" placeholder="email" className="input input-bordered" />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Password</span>
+                                        </label>
+                                        <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                        <label className="label">
+                                            <p className="label-text-alt text-sm">Do not have account ? <span className="text-black font-bold"><Link to={'/register'}>Register</Link></span> </p>
+                                        </label>
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <button onClick={handleGoogleLogIn} className="btn btn-outline btn-neutral"> <FaGooglePlus style={{ fontSize: '30px', marginRight: '10px' }}></FaGooglePlus>Log In</button>
+                                        <input className="btn btn-neutral mt-2" type="submit" value="Login" />
+                                    </div>
+                                    <p className='text-red-600'>{logInError}</p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </form>
+            </div>
             <Footer></Footer>
         </>
 
